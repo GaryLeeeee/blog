84a032a5e50be3b787b2591472a5c5b5c0e7d041
https://github.com/GaryLeeeee/Fellowship-activity.git <664306561@qq.com>
2018-08-29 21:38:33 +0800

    博客编辑及查看

--- src/main/java/com/garylee/blog/BlogApplication.java
+++ src/main/java/com/garylee/blog/BlogApplication.java
@@ -1,9 +1,11 @@
 package com.garylee.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.garylee.blog.dao")
 public class BlogApplication {
 
 	public static void main(String[] args) {
